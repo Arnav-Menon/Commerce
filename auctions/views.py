036cecca4a3b7134144ac8744123afb85403a0d2
Arnav-Menon <arnavmenon@gmail.com>
2020-12-31 17:49:06 -0800

    active listings rendered

--- auctions/views.py
+++ auctions/views.py
@@ -4,11 +4,14 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import *
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = Listing.objects.all()
+    return render(request, "auctions/index.html", {
+        "listing": listings
+    })
 
 
 def login_view(request):
