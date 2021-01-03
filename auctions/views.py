a17dcf52c5a734464495f2b4f58251aa40474c41
Arnav-Menon <arnavmenon@gmail.com>
2021-01-03 00:06:06 -0800

    users can now properly place bids

--- auctions/views.py
+++ auctions/views.py
@@ -4,11 +4,12 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.urls import reverse
 from django import forms
+from django.contrib import messages
 
 from .models import *
 
 class NewBidForm(forms.Form):
-    bid = forms.CharField(max_length=6)
+    bid = forms.IntegerField()
 
 def index(request):
     listings = Listing.objects.all()
@@ -77,28 +78,31 @@ def item_info(request, item_id):
 
     return render(request, "auctions/item.html", {
         "listing": listing,
-        "bid_count": num_bids
+        "bid_count": num_bids,
+        "form": NewBidForm()
     })
 
 # TODO: finish this method
 # form is not rendering prperly
 def place_bid(request, item_id):
-    if request.method == "POST":
-        print("Lets place a bid")
-        form = NewBidForm(request.POST)
-        print(form.is_valid())
-        if form.is_valid():
-            potential_bid = form.cleaned_data["bid"]
-            print(potential_bid)
-            return HttpResponseRedirect(reverse("index"))
-        
-        listings = Listing.objects.get(id=item_id)
-        print(listings)
-        num_bids = listings.listed_item.all().count()
 
-    print("First Time")
+    listing = Listing.objects.get(id=item_id)
+    num_bids = listing.listed_item.all().count()
+
+    form = NewBidForm(request.POST)
+    if form.is_valid():
+        potential_bid = form.cleaned_data["bid"]
+        if potential_bid <= listing.current_price:
+            messages.add_message(request, messages.INFO, "New bid must be greater than current bid. Please try again.")
+        else:    
+            listing.current_price = potential_bid
+            listing.save()
+            # creates and saves object all in one line
+            new_bid = Bid.objects.create(new_price=potential_bid, bid_item=listing)
+            return HttpResponseRedirect(reverse("index"))
+
     return render(request, "auctions/item.html", {
-        "listing": listings,
+        "listing": listing,
         "bid_count": num_bids,
         "form": NewBidForm()
     })
