98e47054f2ad3ce0e0c0add5a29fd0b51afcedf6
Arnav-Menon <arnavmenon@gmail.com>
2021-01-04 19:13:05 -0800

    added path for 'closing' listings

--- auctions/urls.py
+++ auctions/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path("create/", views.create_listing, name="create"),
     path("watchlist/", views.watchlist, name="watchlist"),
     path("list_categories/", views.list_categories, name="list_categories"),
-    path("items_by_category/<str:category>", views.categorical_items, name="cat_items")
+    path("items_by_category/<str:category>", views.categorical_items, name="cat_items"),
+    path("close/<int:item_id>", views.close_listing, name="close_list")
 ]
